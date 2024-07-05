fa444847ac05e6d799203e47379db801b69c0113
Jhan-Paul11 <jhanpaul.bejarano@genstudents.org>
2024-07-05 14:44:59 -0500

    first commit

---
+++ src/main/java/com/example/SAMAY_G6/SamayG6Application.java
@@ -0,0 +1,13 @@
+package com.example.SAMAY_G6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamayG6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamayG6Application.class, args);
+	}
+
+}
